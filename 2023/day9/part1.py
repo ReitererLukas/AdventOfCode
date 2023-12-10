4483ade0ed1eb8e72c181e90f827c4b742d02a22
ReitererLukas <reiluc17@htl-kaindorf.at>
2023-12-10 21:48:40 +0100

    day 9

---
+++ 2023/day9/part1.py
@@ -0,0 +1,39 @@
+
+
+def all_zero(differences: list[int]) -> bool:
+    for num in differences:
+        if num != 0:
+            return False
+    return True
+
+def extrapolate(numbers: list[int]) -> int:
+    difference: list[int] = []
+
+    index: int = 1
+    while index < len(numbers):
+        difference.append(numbers[index]-numbers[index-1])
+        index += 1
+
+    if all_zero(difference):
+        return 0
+    return difference[-1] + extrapolate(difference)
+    pass
+
+def main():
+    f = open("input1.txt", "r")
+    lines: list[str] = f.readlines()
+    f.close()
+
+    res: int = 0
+    for line in lines:
+        numbers: list[int] = list(map(lambda x: int(x), line[:-1].split(" ")))
+        res += extrapolate(numbers) + numbers[-1]
+    print(res)
+    pass
+
+
+if __name__ == "__main__":
+    main()
+    pass
+
+# 1974913025
